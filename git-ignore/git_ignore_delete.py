e86cae3d8b2e6fd576e41b0b7e36bd19513b0b9e
Ciel <imwithye@gmail.com>
2014-03-19 03:16:22 -0700

    fix git ignore delete bug

--- git-ignore/git_ignore_delete.py
+++ git-ignore/git_ignore_delete.py
@@ -6,22 +6,31 @@
 
 # git ignore delete
 
+import os
 from config import USER_PATH
 from file_operation import test_folder
+from file_operation import test_file
 from file_operation import find_all_files
 from file_operation import delete_file
 from file_operation import search_file
 
 def git_ignore_delete(filenames):
+	files = []
 	if len(filenames)==0:
+		path = os.getcwd()+"/.gitignore"
+		if test_file(path):
+			delete_file(path)
+		else:
+			print "no .gitignore found in current directory"
 		return
 
-	files = []
 	test_folder(USER_PATH)
 	find_all_files(USER_PATH, files)
 	delete_files = []
 	for filename in filenames:
-		delete_files.append(search_file(filename, files))
+		path = search_file(filename, files)
+		if path!="":
+			delete_files.append()
 
 	for delete in delete_files:
 		delete_file(delete)
